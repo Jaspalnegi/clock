6a3c8c0a9ed46c57b79b0a063721d9b5173e5ae4
Jaspalnegi <jaspalsingh_bca21_24@its.edu.in>
2024-08-25 23:30:02 +0530

    P

---
+++ first.java
@@ -0,0 +1,35 @@
+// class first{
+      
+//     public static void main(String argv[])
+//     {
+//         System.out.print(" jaspal \n");
+//         System.out.print("pinkey");
+//     }
+// }
+
+// public class first{
+
+//      public static void main(String argv[])
+//      {
+//         System.out.print("*\n");
+//         System.out.print("** \n");
+//         System.out.print("*** \n");
+//         System.out.print("**** \n");
+//      }
+// }
+
+// variable
+
+public class first{
+
+    public static void main(String args[]){
+
+        int a=42;
+        int b=39;
+        // int sum=a+b;
+        int mul =a*b;
+        // System.out.print(sum);
+        System.out.println(mul);
+
+    }
+}
